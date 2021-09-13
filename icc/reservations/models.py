de69ea4670bc51fa89fded297d090c1345fc6fd5
FERREIRA_STO-ESIG <FERREIRA_STO-ESIG@ge-pedago.etat-ge.ch>
2021-09-13 11:06:23 +0200

    + class reservations

--- icc/reservations/models.py
+++ icc/reservations/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime, date
 
 # Create your models here.
+
+
+class Reservation(models.Model):
+    commentaire = models.TextField(blank=True, default='')
+    date = models.CharField(max_length=255)
+    heure = models.TimeField()
