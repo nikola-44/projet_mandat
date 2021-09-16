7b8a72bed9c558b3cdaa77ca0f672f425d983884
dorianchtln <dorian.chtln@eduge.ch>
2021-09-16 16:02:50 +0200

    Revert "Merge branch 'main' of https://github.com/nikola-44/projet_mandat into main"
    This reverts commit e72292c02a0ca6c0ba427219569c8e3522b9398e, reversing
    changes made to b7c460d46241c7ef6f43c36fdd561ee3142943c3.

--- icc/icc/settings.py
+++ icc/icc/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -25,6 +26,7 @@ SECRET_KEY = 'django-insecure-2n_@mu4fga4+(z68$djyr1ea64zieb74pma@0w1e^h#j=p%q!_
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 # Application definition
 
@@ -68,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'icc.wsgi.application'
 
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -77,6 +80,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -96,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -109,11 +114,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
