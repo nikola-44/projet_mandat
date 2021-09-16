b7c460d46241c7ef6f43c36fdd561ee3142943c3
dorianchtln <dorian.chtln@eduge.ch>
2021-09-16 15:58:10 +0200

    Inscription + login environ 65% du travail

--- icc/compte/views.py
+++ icc/compte/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreerUtilisateur
 
 def inscriptionPage(request):
-    context={}
+    form=CreerUtilisateur()
+    if request.method=='POST':
+        form=CreerUtilisateur(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('acces')
+    context={'form':form}
     return render(request,'inscription.html', context)
 
 def accesPage(request):
     context={}
     return render(request,'acces.html', context)
-# Create your views here.
+
