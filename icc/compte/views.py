a531a728da7bf253f82cd37dd55d6ad689eb9d0f
faton <faton.zmr@eduge.ch>
2021-09-17 11:34:47 +0200

    modif

--- icc/compte/views.py
+++ icc/compte/views.py
@@ -4,24 +4,26 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from .forms import CreerUtilisateur
 
+
 def inscriptionPage(request):
-    form=CreerUtilisateur()
-    if request.method=='POST':
-        form=CreerUtilisateur(request.POST)
+    form = CreerUtilisateur()
+    if request.method == 'POST':
+        form = CreerUtilisateur(request.POST)
         if form.is_valid():
             form.save()
             return redirect('acces')
-    context={'form':form}
-    return render(request,'inscription.html', context)
+    context = {'form': form}
+    return render(request, 'inscription.html', context)
+
 
 def accesPage(request):
-    context={}
-    if request.method=='POST':
-        username=request.POST.get('username')
-        password=request.POST.get('password')
-        user=authenticate(request, username= username, password= password)
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('accueil')
 
-    return render(request,'acces.html', context)
+    return render(request, 'acces.html', context)
