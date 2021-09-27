309c02febbb8c6428845b0e8cb96b3c6b9fa5744
faton-zmr <faton.zmr@eduge.ch>
2021-09-27 11:23:35 +0200

    Update settings.py

--- icc/icc/settings.py
+++ icc/icc/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'icc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['../templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static")
 # ]
