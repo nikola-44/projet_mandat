ff5568597d9b256800b35c55b44bd45acf854193
faton-zmr <faton.zmr@eduge.ch>
2021-09-27 11:34:21 +0200

    Update settings.py

--- icc/icc/settings.py
+++ icc/icc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2n_@mu4fga4+(z68$djyr1ea64zieb74pma@0w1e^h#j=p%q!_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
