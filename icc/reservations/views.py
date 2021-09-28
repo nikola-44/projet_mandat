e4144706ee9dc55fdb4cdecb6a01cee12188c631
Nikola-edu <84766227+Nikola-edu@users.noreply.github.com>
2021-09-28 10:42:25 +0200

    = affichage prestations

--- icc/reservations/views.py
+++ icc/reservations/views.py
@@ -11,6 +11,7 @@ from .models import Reservation, Prestation
 def planning(request):
     return render(request, 'reservations/planning.html')
 
+
 # @app.route('/reservations/<string:jours>', methods=['GET'])
 def test(request, jours):
     r = Reservation.objects.all().filter(date=jours).order_by('heure')
@@ -21,7 +22,38 @@ def test(request, jours):
 
 def test_prestations(request):
     prestations = Prestation.objects.all()
-    return render(request, 'reservations/test-prestations.html', {'prestations': prestations})
+
+    noms = []
+    for prestation in prestations:
+        if prestation.pour != '/':
+            if prestation.nom in noms:
+                pass
+            else:
+                noms.append(prestation.nom)
+    # print(noms)
+
+    prix = []
+
+    for nom in noms:
+        objects = Prestation.objects.all().filter(nom=nom)
+        p = []
+        for o in objects:
+            p.append(o.prix)
+        tup = tuple(p)
+        # print(nom)
+        # print(tup)
+        # print(nom + ' - ' + tup.__str__())
+        prix.append(tup)
+    # print(prix)
+
+    dico = {}
+    cpt = 0
+    for nom in noms:
+        dico[noms[cpt]] = (prix[cpt])
+        cpt += 1
+    print(dico)
+
+    return render(request, 'reservations/test-prestations.html', {'prestations': prestations, 'dico': dico})
 
 
 def reserver(request):
