014e5d5632b4f6607e11b68a8e729dddee77cd52
Nikola-edu <84766227+Nikola-edu@users.noreply.github.com>
2021-09-28 10:51:52 +0200

    tset

--- icc/reservations/views.py
+++ icc/reservations/views.py
@@ -53,7 +53,7 @@ def test_prestations(request):
         cpt += 1
     print(dico)
 
-    return render(request, 'reservations/prestations.html', {'prestations': prestations, 'dico': dico})
+    return render(request, 'reservations/test-prestations.html', {'prestations': prestations, 'dico': dico})
 
 
 def reserver(request):
