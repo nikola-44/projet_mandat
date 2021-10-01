f9cb1971a6b8791fb998e22ae4f4b333e9d86329
dorianchtln <89833953+dorianchtln@users.noreply.github.com>
2021-10-01 08:28:55 +0200

    avancée sur gérerProduit coter admin

--- icc/produits/urls.py
+++ icc/produits/urls.py
@@ -8,4 +8,7 @@ app_name = 'produits'
 urlpatterns = [
     path('', views.home, name='home'),
     path('gererProduits/', views.gererProduit, name='gererProduit'),
+    path('supprimer_produit/<str:pk>', views.supprimer_produit, name='supprimer_produit'),
+    path('ajouter_produit/', views.ajouter_produit, name='ajouter_produit'),
+
 ]
--- icc/produits/views.py
+++ icc/produits/views.py
@@ -1,5 +1,5 @@
 # Zumeri Faton et Châtelain Dorian
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Produit
 
 
@@ -13,3 +13,23 @@ def home(request):
 def gererProduit(request):
     produits = Produit.objects.all()
     return render(request, '../templates/gererProduit.html', {'produits': produits})
+
+
+def ajouter_produit(request):
+    form = Produit()
+    if request.method == 'POST':
+        form = Produit(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/produits/gererProduits')
+    context = {'form': form}
+    return render(request, '../templates/ajouter_produit.html', context)
+
+
+def supprimer_produit(request, pk):
+    produit = Produit.objects.get(id=pk)
+    if request.method == 'POST':
+        produit.delete()
+        return redirect('/produits/gererProduits')
+    context = {'item': produit}
+    return render(request, '../templates/supprimer_produit.html', context)
