ab46a8feaa60f627076b90e24cfd29dbf9880312
Nikola-edu <84766227+Nikola-edu@users.noreply.github.com>
2021-10-05 14:09:49 +0200

    if staff

--- icc/reservations/forms.py
+++ icc/reservations/forms.py
@@ -5,6 +5,10 @@ from . import models
 
 
 class Rendezvous(forms.ModelForm):
+    # date = forms.DateField()
+    # heure = forms.TimeField()
+    # commentaire = forms.CharField(widget=forms.Textarea())
+
     class Meta:
         model = models.Reservation
         fields = ['date_heure', 'commentaire']
