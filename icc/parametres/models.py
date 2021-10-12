535bccec0bee5d55453591a28227f10b66c841b4
dorianchtln <89833953+dorianchtln@users.noreply.github.com>
2021-10-12 13:30:54 +0200

    commit

--- icc/parametres/models.py
+++ icc/parametres/models.py
@@ -1,5 +1,5 @@
 # Dorian Châtelain
-
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -9,8 +9,16 @@ class Parametres(models.Model):
     stockMinPVente = models.IntegerField()
     prixLivraison = models.IntegerField()
     telephone = models.IntegerField()
-    adresse = models.CharField(max_length=15)
+    adresse = models.CharField(max_length=30)
     nomSalon = models.CharField(max_length=150)
 
     def __str__(self):
         return self.nomSalon
+
+    def save(self, *args, **kwargs):
+        if not self.pk and Parametres.objects.exists():
+        # if you'll not check for self.pk
+        # then error will also raised in update of exists model
+            raise ValidationError("Il ne peut y avoir qu'un seul parametre")
+        if self.pk:
+            return super(Parametres, self).save(*args, **kwargs)
