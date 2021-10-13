cb4bec0ee2ae9d90eddd5d33a1790ab132f725f7
dorianchtln <89833953+dorianchtln@users.noreply.github.com>
2021-10-13 14:56:50 +0200

    GererSalon 80% terminé + dosssier construction 2 commencé

--- icc/parametres/forms.py
+++ icc/parametres/forms.py
@@ -7,4 +7,4 @@ from .models import Parametres
 class SalonForm(forms.ModelForm):
     class Meta:
         model = Parametres
-        fields = ['stockMinPVente', 'prixLivraison', 'telephone', 'adresse', 'nomSalon']
+        fields = ['stockMinPVente', 'prixLivraison', 'telephone', 'adresse','codePostal','mail', 'nomSalon']
