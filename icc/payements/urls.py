ae1f2fb74af3b322a29dcbc9458d22f0f67f89b7
dorianchtln <89833953+dorianchtln@users.noreply.github.com>
2021-10-19 14:10:33 +0200

    Revert "df"
    This reverts commit b25eb81059634d136ccaeba4c5ecaecff341ac52.

--- icc/payements/urls.py
+++ icc/payements/urls.py
@@ -4,6 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CheckoutPageView.as_view(), name='checkout'),
-    path('charge/', views.charge, name='charge')
+    path('', views.HomePageView.as_view(), name='checkout'),
 ]
--- icc/payements/views.py
+++ icc/payements/views.py
@@ -1,28 +1,7 @@
-import stripe
 from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.base import TemplateView
-from django.conf import settings
 
-stripe.api_key = settings.STRIPE_SECRET_KEY
-
-
-class CheckoutPageView(TemplateView):
-    template_name = '../produits/templates/checkout.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(CheckoutPageView, self).get_context_data(**kwargs)
-        context['key'] = settings.STRIPE_PUBLISHABLE_KEY
-        return context
-
-
-def charge(request):
-    if request.method == 'POST':
-        charge = stripe.Charge.create(
-            amount=500,
-            currency='chf',
-            description='A Django charge',
-            source=request.POST['stripeToken']
-        )
-        return render(request, '../produits/templates/charge.html')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
