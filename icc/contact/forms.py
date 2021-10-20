e5b24ddbf0fae6ee7ec05ad703fd9f7121ab3484
Faton-edu <elv-faton.zmr@eduge.ch>
2021-10-20 22:22:52 +0200

    Update forms.py

--- icc/contact/forms.py
+++ icc/contact/forms.py
@@ -4,7 +4,6 @@ from .models import Contact
 
 # Create your forms here.
 class Contact(forms.Form):
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
+    name = forms.CharField(max_length=50)
     email_address = forms.EmailField(max_length=150)
     message = forms.CharField(widget=forms.Textarea, max_length=2000)
